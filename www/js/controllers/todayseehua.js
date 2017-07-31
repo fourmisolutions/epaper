0cbb3c2d1f6f20ed14c49517d3af4653fc9878ba
hutingung <hutingung@gmail.com>
2017-07-31 08:51:41 +0800

    SHE-28 - Manually merge the changes from master

--- www/js/controllers/todayseehua.js
+++ www/js/controllers/todayseehua.js
@@ -14,8 +14,8 @@ app.controller('TodayShListController', ['$scope', 'ePaperService', '$state', '$
     };
 }]);
 
-app.controller('TodayShController', ['$scope', '$stateParams', '$timeout', 'ePaperService', '$interval', 'ePaperService', 'GaService', 'GaConstants',
-    function($scope, $stateParams, $timeout, ePaperService, $interval, ePaperService, GaService, GaConstants) {
+app.controller('TodayShController', ['$scope', '$stateParams', '$timeout', 'ePaperService', '$interval', 'ePaperService', 'GaService', 'GaConstants','$cordovaSocialSharing',
+    function($scope, $stateParams, $timeout, ePaperService, $interval, ePaperService, GaService, GaConstants, $cordovaSocialSharing) {
 
     $scope.$on("$ionicView.beforeEnter", function(event, data){
         GaService.trackView(GaConstants.scrnNameTodaySeeHua);
@@ -44,6 +44,25 @@ app.controller('TodayShController', ['$scope', '$stateParams', '$timeout', 'ePap
     $scope.description = news.summary;
     $scope.content = news.content;
     $scope.imageURLs = [];
+    $scope.images = news.image;
+    
+    $scope.shareAnywhere = function() {
+        var message = $scope.description;
+		var subject = $scope.description;
+		var image =  $scope.images;
+		var link = 'http://www.test.com';
+		//var link = "";	
+		
+		$cordovaSocialSharing
+		.share(message, subject, image, link) // Share via native share sheet
+		.then(function(result) {
+		  // Success!
+		  //console.log("Sharing success.");
+		}, function(err) {
+		  // An error occured. Show a message to the user
+		  //console.log("Sharing fail.");
+		});
+	}
 
 
     if (news.image.indexOf(',') >= 0)
